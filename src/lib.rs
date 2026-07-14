60025f6e4acfe4dedc4f0cdab6222a78ec0996e6
Tan Dao <ttdao3@asu.edu>
2026-07-13 18:23:38 -0700

    implement test

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,25 @@
-pub fn search<'a> (_query: &str, _contents: &'a str) -> Vec<&'a str>{
-    unimplemented!()
+pub fn search<'a> (query: &str, contents: &'a str) -> Vec<&'a str>{
+    let mut result: Vec<&str> = Vec::new();
+    for line in contents.lines() {
+        println!("line: {}", line);
+        println!("check: {}", line.contains(query));
+        if line.contains(query) {
+            result.push(line);
+        }
+    }
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]    fn one_result() {
+        let query = "duct";
+        let contents = "\
+        Rust :\
+        safe, fast, productive.\
+        Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+
+    }
 }
